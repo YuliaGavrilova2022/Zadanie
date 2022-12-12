54ff5f4bb16d558d110c1cd1101d29876c8811f7
YuliaGavrilova2022 <gavrilova.yulia.a@yandex.ru>
2022-12-12 21:08:33 +0300

    задание 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val chicken = Herbivores("Chicken", 20.0, "birds")
-        val elephant = Herbivores( "Elephant", 10000.0, "mammals")
-        val dog = Predator( "Dog", 150.0, "mammals")
-        val cat = Predator( "Cat", 30.0, "mammals")
-
-        val list = listOf(chicken,elephant,dog,cat)
-        println(list)//Log.d
+      
     }
 }
